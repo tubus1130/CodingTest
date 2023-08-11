80e261e532e3937ed82e59428b22e111ac75354d
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-08-11 11:35:18 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        // 4 6 8 9 10
+        for(int i=1; i<=n; i++){
+            int cnt = 0;
+            for(int j=1; j<=i; j++){
+                if(i % j == 0){
+                    cnt++;
+                }
+                if(cnt > 2){
+                    answer++;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
