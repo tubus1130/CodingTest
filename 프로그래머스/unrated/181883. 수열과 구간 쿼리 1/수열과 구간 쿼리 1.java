aece0e699db6e6ac11c5735d1ba72738d842930d
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-08-14 12:36:36 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 2.09 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int[] query : queries){
+            for(int i=query[0]; i<=query[1]; i++){
+                arr[i]++;
+            }
+        }
+        return arr;
+    }
+}
