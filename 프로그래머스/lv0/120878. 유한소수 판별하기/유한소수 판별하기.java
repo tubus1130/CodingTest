46c69c5e4ae1900193c5867a57e805e9ded07ef5
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-08-22 12:23:46 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,25 +1,23 @@
 class Solution {
-    public int solution(int a, int b) {
-        int newB = b / gcd(a, b);
+     public int solution(int a, int b) {
+        int answer = 1;
+        int max = 0;
+        for(int i =1; i<=a; i++){
+            if(a%i ==0 && b%i ==0) max= i;
+        }
 
-        while (newB != 1) {
-            if (newB % 2 == 0) {
-                newB /= 2;
-            } else if (newB % 5 == 0) {
-                newB /= 5;
-            } else {
+        b /= max;
+
+        while(b>1){
+            if(b%2==0){
+                b /= 2;
+            }else if(b%5==0){
+                b /= 5;
+            }else {
                 return 2;
             }
         }
 
-        return 1;
-    }
-
-    private int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        } else {
-            return gcd(b, a % b);
-        }
+        return answer;
     }
 }
