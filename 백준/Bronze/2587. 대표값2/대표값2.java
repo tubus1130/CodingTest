a140adaf5e1fe8cb9e9ca3f8ec0de357e6aa7c84
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-08-25 22:54:02 +0900

    [Bronze II] Title: 대표값2, Time: 120 ms, Memory: 12864 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[5];
+		int sum = 0;
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+			sum += arr[i];
+		}
+		
+		Arrays.sort(arr);
+		System.out.println(sum/5);
+		System.out.println(arr[2]);
+	}
+
+}
