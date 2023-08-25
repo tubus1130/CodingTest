9cead76867595fba2f6b2369a55f1225e6595227
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-08-25 18:23:45 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        return sum / (double)arr.length;
+    }
+}
