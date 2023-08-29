cdb2735371deeb692067f3a5d0ead263550a664f
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-08-29 15:33:21 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i=0; i<arr.length; i++){
+            if(s.contains(arr[i])){
+                s = s.replace(arr[i], i+"");
+                i--;
+            }
+        }
+        return Integer.parseInt(s);
+    }
+}
