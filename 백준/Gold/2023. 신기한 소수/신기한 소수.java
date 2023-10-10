7b70d1e0ae878aa73c23a4930ff23d830887ce1d
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-10-10 15:37:03 +0900

    [Gold V] Title: 신기한 소수, Time: 864 ms, Memory: 12868 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+	static int N;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		DFS(2, 1);
+		DFS(3, 1);
+		DFS(5, 1);
+		DFS(7, 1);		
+	}
+	
+	public static void DFS(int number, int jarisu) {
+		if(jarisu == N) {
+			if(isPrime(number)) {
+				System.out.println(number);
+			}
+			return;
+		}
+		
+		for(int i=1; i<10; i++) {
+			if(i%2==0) {
+				continue;
+			}
+			if(isPrime(number*10 + i)) {
+				DFS(number*10+i, jarisu+1);
+			}
+		}
+	}
+	
+	public static boolean isPrime(int num) {
+		for(int i=2; i<=num/2; i++) {
+			if(num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
