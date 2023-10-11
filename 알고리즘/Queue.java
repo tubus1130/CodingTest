0ed6286ed88c236d53b8d9b3bd9ade5851f94f4d
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-10-11 16:19:29 +0900

    Create Queue.java

---
+++ 알고리즘/Queue.java
@@ -0,0 +1,7 @@
+Queue<Integer> queue = new LinkedList<>();
+queue.offer(1);
+queue.offer(2);
+queue.offer(3);
+System.out.println(queue.poll()); 
+System.out.println(queue.poll()); 
+System.out.println(queue.poll()); 
