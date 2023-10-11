89e748d068fcce548ad31016c1e1b45e756ab9bf
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-10-11 16:19:12 +0900

    Create Stack.java

---
+++ 알고리즘/Stack.java
@@ -0,0 +1,7 @@
+Stack<Integer> stack = new Stack<>();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+System.out.println(stack.pop()); 
+System.out.println(stack.pop()); 
+System.out.println(stack.pop()); 
