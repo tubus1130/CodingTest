15009062ce55b4649b16de81e8daf9bcc5709223
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-10-16 14:10:53 +0900

    [Silver IV] Title: 수 찾기, Time: 1884 ms, Memory: 79288 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            boolean find = false;
+            int target = Integer.parseInt(st.nextToken());
+            int start = 0;
+            int end = A.length - 1; 
+            while (start <= end) {
+                int midi = (start + end) / 2;
+                int midV = A[midi];
+                if (midV > target) {
+                    end = midi - 1;
+                } else if (midV < target) {
+                  start = midi + 1;
+                } else {
+                    find = true;
+                    break;
+                }
+            }
+            if (find) {
+              System.out.println(1);
+            }
+            else
+            {
+              System.out.println(0);
+            }
+        }
+	}
+	
+}
