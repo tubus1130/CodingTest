ffc6f8d2ebfe231b72a2ac567a802d39e6d42be1
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-10-25 22:25:08 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 116 ms, Memory: 12844 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,31 +1,32 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
-	
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		
 		int a = sc.nextInt();
 		int b = sc.nextInt();
-		int temp;
+		
 		if(a < b) {
-			temp = a;
+			int temp = a;
 			a = b;
 			b = temp;
 		}
 		
+		// 최대공약수
 		for(int i=b; i>0; i--) {
-			if(a % i == 0 && b % i == 0) {
+			if((a % i == 0) && (b % i == 0)) {
 				System.out.println(i);
 				break;
 			}
 		}
 		
-		for(long i=a; ;i+=a) {
-			if(i % a == 0 && i % b == 0) {
+		// 최소공배수
+		for(long i=a; ; i+=a) {
+			if((i % a == 0) && (i % b == 0)) {
 				System.out.println(i);
 				break;
 			}
 		}
-		
 	}
 }
