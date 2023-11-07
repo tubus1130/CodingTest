aaf59dfa89df1e3ed8233fb35f675d8ef537c3de
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-11-07 22:31:40 +0900

    [Bronze I] Title: 최소공배수, Time: 156 ms, Memory: 12764 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,13 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
-		int T = sc.nextInt();
-		for(int i=0; i<T; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
+		int t = Integer.parseInt(br.readLine());
+		for(int i=0; i<t; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 			
 			if(a < b) {
 				int temp = a;
@@ -15,12 +20,16 @@ public class Main {
 				b = temp;
 			}
 			
-			for(int j=b; ; j+=b) {
-				if((j % a == 0) && (j % b == 0)) {
-					System.out.println(j);
-					break;
-				}
-			}
+			int gcd = getGCD(a, b);
+			System.out.println(a*b/gcd);
 		}
+	}
+
+	private static int getGCD(int a, int b) {
+		if(b == 0) {
+			return a;
+		}
+		return getGCD(b, a%b);
+		
 	}
 }
