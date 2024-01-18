30207bc68e8ec0eeea712e6b87c003c90c0c15bf
김동호 <50819376+tubus1130@users.noreply.github.com>
2024-01-18 22:32:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 39.27 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,18 +1,19 @@
 '''
-리스트에 넣고 정렬한후 반복돌면서 확인하고 pop
+part를 딕셔너리로 만들고, 동명이인은 +=1
+comp 반복돌면서 값 -=1 value가 0이되면 del
+
+string으로 변환해서 딕셔너리의 키 반환
 '''
 def solution(participant, completion):
-    dic = {}
-    
-    for p in participant:
-        if p in dic:
-            dic[p] += 1
+    dic = dict()
+    for part in participant:
+        if part not in dic:
+            dic[part] = 1
         else:
-            dic[p] = 1
-    
-    for c in completion:
-        dic[c] -= 1
-        
+            dic[part] += 1
+    for com in completion:
+        dic[com] -= 1
     for key in dic.keys():
         if dic[key] > 0:
             return key
+    return dic
